322fb1bc070499e31ddc4d08a4df1bff87777d68
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-04-04 20:00:50 -0300

    style button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,9 +1,40 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    width: 200px;
+    height: 50px;
+    display: flex;
+    background-color: #1550FF;
+    border-radius: 10px;
+    cursor: pointer;
+    opacity: 1;
+    transition: all ease .3s;
 
-export const IconArea = styled.div``;
+    &:hover {
+        opacity: .8;
+    }
+`;
+
+export const IconArea = styled.div`
+    height: inherit;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    padding: 0 15px;
+
+    & svg {
+        width: 20px;
+        height: 20px;
+        
+    }
+`;
 
 export const Icon = styled.img``;
 
-export const Label = styled.p``;
+export const Label = styled.p`
+    color:#FFF;
+    height: inherit;
+    display: block;
+    flex: 1;
+`;
