cfd93036d52ce6ac2dbdd05871ffd0e1ea627f92
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-04-04 19:18:55 -0300

    create project structure

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import * as Styled from "./styles";
+
+function App() {
+  return (
+    <Styled.Container>
+      <Styled.Info>
+        <Styled.LogoLink href="">
+          <img src="" alt="" />
+        </Styled.LogoLink>
+        <Styled.InfoArea>...</Styled.InfoArea>
+        <button>REINICIAR</button>
+      </Styled.Info>
+      <Styled.GridArea>...</Styled.GridArea>
+    </Styled.Container>
+  );
+}
+
+export default App;
