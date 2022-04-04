bd8e75d069346cb97463e57559bf8b2181c5ebfc
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-04-04 19:25:28 -0300

    add basic layout container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import * as Styled from "./styles";
+import { ReactComponent as LogoIcon } from "./assets/images/b7.svg";
 
 function App() {
   return (
     <Styled.Container>
       <Styled.Info>
         <Styled.LogoLink href="">
-          <img src="" alt="" />
+          <LogoIcon />
         </Styled.LogoLink>
         <Styled.InfoArea>...</Styled.InfoArea>
         <button>REINICIAR</button>
--- src/styles.tsx
+++ src/styles.tsx
@@ -1,7 +1,38 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
-export const Info = styled.div``;
-export const LogoLink = styled.a``;
-export const InfoArea = styled.div``;
-export const GridArea = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  max-width: 750px;
+  margin: auto;
+  display: flex;
+
+  & svg {
+    width: 50px;
+    height: 50px;
+  }
+
+  @media (max-width: 750px) {
+    flex-direction: column;
+  }
+`;
+export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media (max-width: 750px) {
+    margin-bottom: 50px;
+    align-items: center;
+  }
+`;
+export const LogoLink = styled.a`
+  display: block;
+`;
+export const InfoArea = styled.div`
+  width: 100%;
+  margin: 10px 0;
+`;
+export const GridArea = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+`;
